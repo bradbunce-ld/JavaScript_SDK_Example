d61a75796aee1a89e22432a152183f1a7947f7b5
Brad Bunce <bbunce@launchdarkly.com>
2021-09-09 23:10:40 -0400

    Initial commit

---
+++ data.js
@@ -0,0 +1,13 @@
+var clientID = "6035b0fed97f7509aac2dcab";
+var FeatureFlag = "javascript-sdk-test-flag-1";
+var user = {
+    "key": "671749",
+    "name": "Brad Bunce",
+    "firstName": "Brad",
+    "lastName": "Bunce",
+    "email": "bbunce@launchdarkly.com",
+    "custom": {
+      "company": "LaunchDarkly",
+      "group": "SEA"
+    }
+  };
